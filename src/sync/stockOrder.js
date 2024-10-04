50fd4be8f476f4cc76f53578548e3804130143d7
Titanstic <mgminkhant301999@gmail.com>
2024-10-04 11:42:17 +0630

    [api]: add syn func in calculate stock js

--- src/sync/stockOrder.js
+++ src/sync/stockOrder.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {executeCentralMutation, checkOperation} = require("../utils");
+const poolQuery = require("../../misc/poolQuery");
 const stockOrderRouter = express.Router();
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -7,18 +8,24 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 stockOrderRouter.post("/", async (req, res) => {
     const event = req.body.event;
     const tableName = req.body.table.name;
-    console.log(`[stockOrderRouter] event:`, event);
-    console.log(`[stockOrderRouter] tableName:`, tableName);
 
     try{
         if((tableName === "purchase_order_item" || tableName === "good_return_item" || tableName === "good_received_item") && event.op === "INSERT"){
             await delay(3000);
         }
 
+        if(tableName  === "purchase_order" || tableName === "good_received" || tableName === "good_return"){
+            delete event.data.new.sync;
+        }
+
         // execute data to central
         const {query, variables} = await checkOperation(event, tableName);
-        console.log(`[stockOrderRouter] query:`, query);
-        await executeCentralMutation(query, variables);
+        const centralRes = await executeCentralMutation(query, variables);
+
+        if(!centralRes.errors){
+            const purchaseOrderId = event.data.new.id;
+            await poolQuery(`UPDATE purchase_order SET sync = true WHERE id = $1;`, [purchaseOrderId]);
+        }
 
         console.log(`[stockOrderRouter] :`, "Order Successfully to cloud");
         res.status(200).json({ success: true, message: "Order Successfully to cloud" });
