f12f0ec709494c94a024f143f71375fb5c0d5ca3
Titanstic <mgminkhant301999@gmail.com>
2024-10-28 19:18:03 +0630

    [feature]: get ip from node js

--- src/cashierDrawer/cashierDrawerController.js
+++ src/cashierDrawer/cashierDrawerController.js
@@ -6,8 +6,10 @@ const cashierDrawerController = express.Router();
 
 cashierDrawerController.post("/create", async (req, res) => {
     const {opening_cash, employee_id, posIpAddress } = req.body.input ? req.body.input : req.body;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('Client IP Address:', ip)
     try {
-        const { cashierDrawerData } = await createCashierDrawer(posIpAddress, opening_cash, employee_id);
+        const { cashierDrawerData } = await createCashierDrawer(ip, opening_cash, employee_id);
 
         res.status(200).send({error: 0, message: JSON.stringify(cashierDrawerData[0])});
     } catch (e) {
