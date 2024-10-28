0a4fc3817e872834a8f55af895d1b86cc6102181
Titanstic <mgminkhant301999@gmail.com>
2024-10-28 18:12:21 +0630

    request ip addres from pos

--- src/cashierDrawer/cashierDrawerModal.js
+++ src/cashierDrawer/cashierDrawerModal.js
@@ -2,7 +2,7 @@ const poolQuery = require("../../misc/poolQuery");
 
 
 const createCashierDrawer = async (posIpAddress, opening_cash, employee_id) => {
-    const { rows: cashierDrawerData } = await poolQuery(`INSERT INTO cashier_drawer(opening_cash, opening_employee_id, pos_ip_address) VALUES($1, $2, $3) RETURNING *`, [opening_cash, employee_id, posIpAddress]);
+    const { rows: cashierDrawerData } = await poolQuery(`INSERT INTO cashier_drawer(opening_cash, opening_employee_id, pos_ip_address, cash_in_drawer) VALUES($1, $2, $3, $4) RETURNING *`, [opening_cash, employee_id, posIpAddress, opening_cash]);
     if(cashierDrawerData.length > 0 ){
         console.log(`cashierDrawerModal [createCashierDrawer] cashierDrawerData: `, cashierDrawerData);
         return { cashierDrawerData };
