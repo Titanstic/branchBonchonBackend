d3f1b506f68adf33c5c8542d109aff0dfe95ce3a
Titanstic <mgminkhant301999@gmail.com>
2024-12-08 12:33:03 +0630

    [feature]: stock item current qty sync

--- src/controllers/sync/twoTableSyncController.js
+++ src/controllers/sync/twoTableSyncController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {checkOperation, delay} = require("../../utils/mutation");
 const {executeCentralMutation} = require("../../utils/centralHasuraSync");
 const {filterSyncHistory} = require("../../utils/syncHistory");
-const {findCurrentBranch} = require("../../models/branchModel");
+const {findCurrentBranch, findBranch} = require("../../models/branchModel");
 
 const twoTableHasuraSyncRouter = express.Router();
 
@@ -28,7 +28,6 @@ twoTableHasuraSyncRouter.post("/hasura-sync", async (req, res) => {
 
     try{
         const centralRes = await executeCentralMutation(query, variables);
-        console.log(centralRes);
 
         console.log(`[twoTableHasuraSyncRouter] :`, "Sync Successfully");
         res.status(200).json({ success: true, message: "Sync Successfully" });
@@ -40,5 +39,32 @@ twoTableHasuraSyncRouter.post("/hasura-sync", async (req, res) => {
     }
 });
 
+twoTableHasuraSyncRouter.post("/branchStock", async (req, res) => {
+    const event = req.body.event;
+    const inputData = event.data.new;
+
+    const query = `
+            mutation MyMutation($qty: float8!, $branchId: uuid!, $stockItemId: uuid!) {
+                  update_branch_stock_items(where: {branch_id: {_eq: $branchId}, stock_item_id: {_eq: $stockItemId}}, _set: {qty: $qty}) {
+                        affected_rows
+                  }
+            }
+        `;
+
+    try{
+        const branchData = await findBranch();
+
+        const currentQty = inputData.recipe_unit_id ? inputData.current_qty / inputData.recipe_qty : inputData.current_qty;
+        const variables = { qty: currentQty, branchId: branchData.id, stockItemId: inputData.id }
+
+        await executeCentralMutation(query, variables);
+
+        console.log(`[twoTableHasuraSyncRouter] branchStock :`, "Branch stock item Sync Successfully");
+        res.status(200).json({ success: true, message: "Branch stock item Sync Successfully" });
+    }catch (e) {
+        console.error(`[twoTableHasuraSyncRouter] branchStock Error:`, e.message);
+        res.status(500).json({ success: false, message: e.message });
+    }
+})
 
 module.exports = twoTableHasuraSyncRouter;
